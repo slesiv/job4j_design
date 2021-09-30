46bc01a41fe12255eea7c829aca98a36ba16717e
Sles Ilia <slesilya@mail.ru>
2021-10-01 01:12:08 +0300

    Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Analizy {
+    public static void unavailable(String source, String target) {
+        boolean errorStart = false;
+        int code;
+        String dateTimeStart = "";
+        String dateTimeEnd;
+        String lineLog;
+
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+                while ((lineLog = in.readLine()) != null) {
+                    if (!lineLog.isEmpty()) {
+                        code = Integer.parseInt(lineLog.substring(0, 3));
+                        if (code == 500 || code == 400) {
+                            if (!errorStart) {
+                                dateTimeStart = lineLog.substring(4);
+                                errorStart = true;
+                            }
+                        } else if (errorStart) {
+                            dateTimeEnd = lineLog.substring(4);
+                            errorStart = false;
+                            out.write(dateTimeStart + ";" + dateTimeEnd + ";");
+                            out.write(System.lineSeparator());
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        unavailable("logError.txt", "target.txt");
+    }
+}

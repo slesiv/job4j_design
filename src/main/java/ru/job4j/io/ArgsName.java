fe404d85f38489f4e5b00b0c55f1bc006fe13dc4
Sles Ilia <slesilya@mail.ru>
2021-11-05 00:39:34 +0300

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,53 @@
+package ru.job4j.io;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ArgsName {
+
+    private Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("parameters missing");
+        } else {
+            values = Arrays.stream(args).filter(x -> {
+                if (!x.contains("-")) {
+                    throw new IllegalArgumentException("The parameter is specified incorrectly, the \"-\" symbol is missing");
+                } else if (!x.contains("=")) {
+                    throw new IllegalArgumentException("The parameter is specified incorrectly, the \"=\" symbol is missing");
+                }
+                return true; })
+            .map(str -> str.substring(1).split("="))
+            .collect(Collectors.toMap(s -> {
+                if (s[0].isEmpty()) {
+                    throw new IllegalArgumentException("Key should not contains null");
+                }
+                return s[0];
+            }, s -> {
+                if (s.length <= 1) {
+                    throw new IllegalArgumentException("Value should not contains null");
+                }
+                return s[1];
+            }));
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}

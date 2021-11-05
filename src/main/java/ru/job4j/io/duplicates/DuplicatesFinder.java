7a854e2876d83392d7927629d750a39b275d39e8
Sles Ilia <slesilya@mail.ru>
2021-11-05 16:48:50 +0300

    4.2. Поиск дубликатов [#315066 #224618]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -1,16 +1,16 @@
 package ru.job4j.io.duplicates;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor dv = new DuplicatesVisitor();
-        Files.walkFileTree(Path.of("./"), dv);
 
-        dv.getFiles().entrySet().stream()
-                .filter(mp -> mp.getValue().size() > 1)
-                .forEach(System.out::println);
+        dv.createMapDuplicates("./").entrySet().stream()
+                .forEach(entry -> {
+                    System.out.println(entry.getKey());
+                    entry.getValue().forEach(System.out::println);
+                    System.out.println();
+                });
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -2,29 +2,38 @@ package ru.job4j.io.duplicates;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private final Map<FileProperty, List<Path>> files = new HashMap<>();
+    private final Map<FileProperty, List<Path>> mapFileAndPaths = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty fileProp = new FileProperty(file.toFile().length(), file.toFile().getName());
-        if (!files.containsKey(fileProp)) {
-            List<Path> paths = new ArrayList<>();
-            paths.add(file.toAbsolutePath());
-            files.put(fileProp, paths);
-        } else {
-            files.get(fileProp).add(file.toAbsolutePath());
-        }
+        putMapFaP(file);
         return super.visitFile(file, attrs);
     }
 
-    public Map<FileProperty, List<Path>> getFiles() {
-        return files;
+    public Map<FileProperty, List<Path>> createMapDuplicates(String start) throws IOException {
+        Files.walkFileTree(Path.of(start), this);
+        return mapFileAndPaths.entrySet().stream()
+                .filter(entry -> entry.getValue().size() > 1)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private void putMapFaP(Path file) {
+        FileProperty fileProp = new FileProperty(file.toFile().length(), file.toFile().getName());
+        if (!mapFileAndPaths.containsKey(fileProp)) {
+            List<Path> paths = new ArrayList<>();
+            paths.add(file.toAbsolutePath());
+            mapFileAndPaths.put(fileProp, paths);
+        } else {
+            mapFileAndPaths.get(fileProp).add(file.toAbsolutePath());
+        }
     }
 }

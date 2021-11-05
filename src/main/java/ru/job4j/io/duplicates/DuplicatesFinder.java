b90d59e1be4fef33266acdf101cff490de8ac240
Sles Ilia <slesilya@mail.ru>
2021-11-05 16:49:58 +0300

    4.2. Поиск дубликатов [#315066 #224618]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,11 +6,10 @@ public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor dv = new DuplicatesVisitor();
 
-        dv.createMapDuplicates("./").entrySet().stream()
-                .forEach(entry -> {
-                    System.out.println(entry.getKey());
-                    entry.getValue().forEach(System.out::println);
-                    System.out.println();
-                });
+        dv.createMapDuplicates("./").forEach((key, value) -> {
+            System.out.println(key);
+            value.forEach(System.out::println);
+            System.out.println();
+        });
     }
 }

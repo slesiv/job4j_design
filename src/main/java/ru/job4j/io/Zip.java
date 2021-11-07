8af4bc566b03b2a49e05a97c27fe8f53606861da
Sles Ilia <slesilya@mail.ru>
2021-11-07 12:56:48 +0300

    5.2. Архивировать проект [#861 #225436]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -13,10 +13,7 @@ public class Zip {
     private final String directoryForPack;
 
     public static void main(String[] args) {
-        if (args.length != 3) {
-            throw new IllegalArgumentException("include all arguments -d=\"directory for packing\" -o=\"name for zip\" -e=\"extensions file fo exclude(txt,xml,...)\"");
-        }
-        ArgsName argsName = ArgsName.of(args);
+        ArgsName argsName = validationArgs(args);
         Zip zip = new Zip(argsName.get("o"), argsName.get("d"), argsName.get("e").split(","));
         zip.packZip();
         System.out.println("zip successfully packing");
@@ -30,14 +27,10 @@ public class Zip {
 
     public void packZip() {
         File file = new File(directoryForPack);
-        if (file.isDirectory()) {
-            try (ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(nameZip)))) {
-                packDir(Arrays.asList(file.listFiles()), zos);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            throw new IllegalArgumentException("Not found folder \"" + directoryForPack + "\"");
+        try (ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(nameZip)))) {
+            packDir(Arrays.asList(file.listFiles()), zos);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -88,5 +81,28 @@ public class Zip {
             return "";
         }
         return file.substring(index + 1);
+    }
+
+    private static ArgsName validationArgs(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("The following arguments are available and required: \n-d=\"directory for packing\", \n-o=\"name for zip\", \n-e=\"extensions file fo exclude(txt,xml,...)\", \nNo other arguments are accepted.\n");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        isValidArg(argsName, "d", "-d=\"directory for packing\"");
+        isValidArg(argsName, "o", "-o=\"name for zip\"");
+        isValidArg(argsName, "e", "-e=\"extensions file fo exclude(txt,xml,...)\"");
+
+        File d = new File(argsName.get("d"));
+        if (!d.isDirectory()) {
+            throw new IllegalArgumentException("Not found folder \"" + d + "\"");
+        }
+        return argsName;
+    }
+
+    private static void isValidArg(ArgsName argsName, String arg, String msgError) {
+        String necessary = "Is necessary argument ";
+        if (argsName.get(arg) == null) {
+            throw new IllegalArgumentException(necessary + msgError);
+        }
     }
 }

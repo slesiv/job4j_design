afd37425c7d672e224267dc96f32de77c5c8facc
Sles Ilia <slesilya@mail.ru>
2021-11-17 22:05:49 +0300

    3. Slf4j - вывод переменных. [#268852 #229421]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,7 +8,6 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        String firstName = "Ilia";
         boolean married = true;
         byte countPets = 1;
         short age = 29;
@@ -16,7 +15,8 @@ public class UsageLog4j {
         long tel = 9012223344L;
         float height = 175.8f;
         double weight = 74.55;
+        char sex = 'M';
 
-        LOG.debug("About me(lie), name: {}, married: {}, countPets: {}, age: {}, region: {}, tel: {}, height: {}, weight: {}", firstName, married, countPets, age, region, tel, height, weight);
+        LOG.debug("About me(lie), sex: {}, married: {}, countPets: {}, age: {}, region: {}, tel: {}, height: {}, weight: {}", sex, married, countPets, age, region, tel, height, weight);
     }
 }

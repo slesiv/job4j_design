bdccd06b02767f10437857f479f152fd9ece2bf9
Sles Ilia <slesilya@mail.ru>
2021-12-23 22:51:50 +0300

    0. Что такое база данных. [#6860 #242691]

--- data/createUpdateDelete.sql
+++ data/createUpdateDelete.sql
@@ -3,10 +3,13 @@ create database company;
 create table persons(
     id serial primary key,
     first_name varchar(50),
-    age smallint
+    age smallint,
+    birth date
 );
 
 
 insert into persons(first_name, age) values ('Григорий', '33');
 
 update persons set first_name='Илья';
+
+delete from persons;

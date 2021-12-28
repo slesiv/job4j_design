091cb37e409b1c82c2b9c5a98b56f72ec97df225
Sles Ilia <slesilya@mail.ru>
2021-12-29 00:06:33 +0300

    0. Select [#6861]

--- data/sqlExample/select/select.sql
+++ data/sqlExample/select/select.sql
@@ -1,8 +1,14 @@
+insert into fauna (name, avg_age, discovery_date)
+values
+('fish', 15000, null),
+('cat', 30000, '01.04.1567'),
+('dog', 33000, '21.07.1476'),
+
 select * from fauna
 where name like '%fish%';
 
 select * from fauna
-where avg_age > 10000 and avg_age < 21000;
+where avg_age between 10000 and 21000;
 
 select * from fauna
 where discovery_date is null;

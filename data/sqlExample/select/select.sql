79e286f8408354a299034cdfcd76777623c074cd
Sles Ilia <slesilya@mail.ru>
2021-12-29 00:06:58 +0300

    0. Select [#6861]

--- data/sqlExample/select/select.sql
+++ data/sqlExample/select/select.sql
@@ -2,7 +2,7 @@ insert into fauna (name, avg_age, discovery_date)
 values
 ('fish', 15000, null),
 ('cat', 30000, '01.04.1567'),
-('dog', 33000, '21.07.1476'),
+('dog', 33000, '21.07.1476');
 
 select * from fauna
 where name like '%fish%';

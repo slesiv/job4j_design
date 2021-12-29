9bbb71ec46e394fd4e20867f7f35dccbba50fa7e
Sles Ilia <slesilya@mail.ru>
2021-12-29 23:03:40 +0300

    2. Группировка и агрегатные функции [#373618 #245315]

--- data/sqlExample/groupAndAgrFunc/groupAndAgrFunc.sql
+++ data/sqlExample/groupAndAgrFunc/groupAndAgrFunc.sql
@@ -1,18 +1,3 @@
-select avg(price) from devices;
-
-select p.name, avg(d.price)
-from devices_people as dp
-join devices as d on dp.device_id = d.id
-join people as p on dp.people_id = p.id
-group by p.name;
-
-select p.name, avg(d.price)
-from devices_people as dp
-join devices as d on dp.device_id = d.id
-join people as p on dp.people_id = p.id
-group by p.name
-having avg(d.price) > 700;
-
 insert into devices (name, price)
 values
 ('phone', 1000),
@@ -63,3 +48,18 @@ values
 (8, 6),
 (8, 5),
 (9, 6);
+
+select avg(price) from devices;
+
+select p.name, avg(d.price)
+from devices_people as dp
+join devices as d on dp.device_id = d.id
+join people as p on dp.people_id = p.id
+group by p.name;
+
+select p.name, avg(d.price)
+from devices_people as dp
+join devices as d on dp.device_id = d.id
+join people as p on dp.people_id = p.id
+group by p.name
+having avg(d.price) > 700;
